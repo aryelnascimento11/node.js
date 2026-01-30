4e808b8ddf26f4c521342ab3e5e84653a36cff08
Aryel Correa do Nascimento <126723967+aryelnascimento11@users.noreply.github.com>
2026-01-30 14:05:09 -0300

    next.js

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <div>
+      <aside>
+        <div>
+          <div id="iconi"></div>
+          <span>bracov</span>
+        </div>
+
+        <nav>
+          <a href="">sei la oque</a>
+          <a href="">sei la oque</a>
+          <a href="">sei la oque</a>
+          <a href="">sei la oque</a>
+        </nav>
+      </aside>
+      <main>
+        <header className="flex-2 text-4xl font-bold text-center">
+          <h1>ola sei la oq</h1>
+          <p>Este é o conteúdo da página principal.</p>
+        </header>
+        <div>
+          <img src="" alt="" />
+          <h1>projeto 1</h1>
+          <p>asjdsadisadsadadw</p>
+          <button>iniciar</button>
+        </div>
+      </main>
+    </div>
+  );
+}
